921afc20ab58b92e1fd4b5f96852d80f12c0152e
Lucas <lucas.rr@live.com>
2025-01-31 16:43:37 -0400

    intent fix

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions
     ]
 });
 
